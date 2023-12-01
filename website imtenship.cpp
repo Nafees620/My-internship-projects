239b7cda700340a4e57da1d86bbfe08423296714
Nafees620 <152601185+Nafees620@users.noreply.github.com>
2023-12-01 14:46:15 +0530

    Add files via upload

---
+++ website imtenship.cpp
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Business Name</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f5f5f5;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em;
+        }
+
+        section {
+            margin: 2em;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        p {
+            line-height: 1.6;
+            color: #666;
+        }
+
+        .key-offerings {
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+        }
+
+        .offering {
+            width: 30%;
+            background-color: #fff;
+            padding: 1em;
+            margin: 1em;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .cta-button {
+            display: inline-block;
+            padding: 0.8em 1.5em;
+            margin-top: 1em;
+            text-decoration: none;
+            color: #fff;
+            background-color: #333;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+
+        .cta-button:hover {
+            background-color: #555;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Your Business Name</h1>
+    </header>
+
+    <section>
+        <h2>Elevate Your Experience with Us</h2>
+        <p>At Your Business Name, we are dedicated to providing top-notch solutions that redefine excellence.</p>
+
+        <div class="key-offerings">
+            <div class="offering">
+                <h3>Product/Service 1</h3>
+                <p>Description of the first product or service. Highlight its key features, benefits, and how it stands out in the market.</p>
+                <a href="#" class="cta-button">Learn More</a>
+            </div>
+
+            <div class="offering">
+                <h3>Product/Service 2</h3>
+                <p>Description of the second product or service. Emphasize its value proposition and how it complements the first offering.</p>
+                <a href="#" class="cta-button">Discover</a>
+            </div>
+
+            <div class="offering">
+                <h3>Product/Service 3</h3>
+                <p>Description of the third product or service. Illustrate how it completes the trio of exceptional solutions.</p>
+                <a href="#" class="cta-button">Explore</a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
